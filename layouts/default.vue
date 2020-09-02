6b33e2354266631dd03b639c773fc84f0d185ee0
krimson998 <orangefilin@gmail.com>
2020-09-02 16:00:28 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app color="indigo" dark>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+    <Nuxt />
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-main>
+    <v-container class="fill-height bg-white" fluid> </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
